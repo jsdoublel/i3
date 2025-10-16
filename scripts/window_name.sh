7945b052a239bf226b76632841ededf462b31351
James Willson <jamessw156@gmail.com>
2025-10-16 10:36:31 -0500

    fixed window name issue

--- scripts/window_name.sh
+++ scripts/window_name.sh
@@ -4,16 +4,22 @@
 
 MAX_LEN=40
 
-trunc() { 
+trunc() {
 	(( ${#1} >= MAX_LEN )) && echo "$(echo "$1" | head -c $MAX_LEN)..." || echo "$1"
 }
 
-name=$(xdotool getwindowfocus getwindowname | iconv -f UTF-8 -t ASCII -c)
-class=$(xdotool getwindowfocus getwindowclassname | iconv -f UTF-8 -t ASCII -c)
+# wid=$(xdotool getwindowfocus 2>/dev/null)
+# [[ -z $wid || $wid == 0 ]] && exit 0
+
+# name=$(xdotool getwindowname "$wid" 2>/dev/null | iconv -f UTF-8 -t ASCII -c)
+# class=$(xdotool getwindowclassname "$wid" 2>/dev/null | iconv -f UTF-8 -t ASCII -c)
+name=$(i3-msg -t get_tree | jq -r '.. | objects | select(.focused? == true) | .name')
+class=$(i3-msg -t get_tree | jq -r '.. | objects | select(.focused? == true) | .window_properties.class')
+[[ -z $name && -z $class ]] && exit 0
+
 [[ -n $class && "${name^^}" == *"${class^^}" ]] && name=${name% -*}
-if [[ ! -z $class ]]; then  
-	printf '%s' "${class^^}" 
-	[[ "${name^^}" != "${class^^}" ]] && printf ' [%s]' "$(trunc "$name")"
+if [[ -n $class && ${class^^} != "NULL" ]]; then
+	printf '%s' "${class^^}"
+	[[ "${name^^}" != "${class^^}" && -n $name ]] && printf ' [%s]' "$(trunc "$name")"
 fi
 printf '\n'
-
